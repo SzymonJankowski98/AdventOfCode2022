5669de3ca750fe1b5c0604b16aa85dd29b6d61de
SzymonJankowski98 <jankowski.szymon.98@gmail.com>
2023-02-22 20:16:06 +0100

    Add day1 part2

---
+++ day1/part2.exs
@@ -0,0 +1,19 @@
+defmodule Calories do
+  def calculate(lines) do
+    lines
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.reduce(0, &+/2)
+  end
+end
+
+{:ok, data} = File.read("input.txt")
+
+result = data
+  |> String.split(~r/\n\n|\r\n\r\n/)
+  |> Enum.map(&String.split/1)
+  |> Enum.map(&Calories.calculate/1)
+  |> Enum.sort
+  |> Enum.take(-3)
+  |> Enum.sum
+
+IO.inspect(result)
